0171c0df0c45b16ee3b3aafd61d222e465c9399e
ccanutinho01 <ccanutinho01@gmail.com>
2026-07-02 19:25:12 -0300

    atualização do codigo

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,19 +1,44 @@
-import {useEffect,} from "react";
+﻿import {useEffect,useState} from "react";
+
 function App() {
+    const [cidade, setCidade] = useState("");
+    const [cep, setCep] = useState("");
+    const [endereco, setEndereco] = useState("");
+    const [estado, setEstado] = useState("");
+    const [inputCep, setInputCep] = useState("");
+
     useEffect(() => {
 
         fetch("https://viacep.com.br/ws/49045000/json/")
         .then(res => res.json())
         .then(data => {
-            console.log(data);
+            setCidade(data.localidade);
+            setCep(data.cep);
+            setEndereco(data.logradouro);
+            setEstado(data.uf);
         });
     }, []);
     return (
         <div>
-            <h1>Consulta CEP</h1>  
+            <h2>Cidade:</h2>
+            <p>{cidade}</p>
+
+            <h3>CEP:</h3>
+            <p>{cep}</p>
+
+            <h4>Endereço:</h4>
+            <p>{endereco}</p>
+            
+            <h5>Estado:</h5>
+            <p>{estado}</p>
+
+
+
+            <input type="text" value={cep} onChange={(e) => setCep(e.target.value)} />
+
+
         </div>
     );
-
 }
 
 
