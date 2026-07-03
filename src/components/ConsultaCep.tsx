7c3a27616cad6b891ddaa2b08946182ee5beeb55
ccanutinho01 <ccanutinho01@gmail.com>
2026-07-03 19:09:05 -0300

    atualização do codigo

---
+++ src/components/ConsultaCep.tsx
@@ -0,0 +1,20 @@
+﻿import { useEffect, useState } from "react";
+
+async function consultaCEP() {
+    const resposta = await fetch("https://viacep.com.br/ws/49045010/json/");
+    const dados = await resposta.json();
+    console.log(dados);
+    return dados;
+}
+
+const ConsultaCep = () => {
+    const [dados, setDados] = useState(null);
+
+    useEffect(() => {
+        consultaCEP().then(setDados);
+    }, []);
+
+    return <div>{dados ? JSON.stringify(dados) : "Carregando..."}</div>;
+};
+
+export default ConsultaCep;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
-import App from './App';
+import ConsultaCep from './ConsultaCep';
 
 const Home: React.FC = () => {
   return (
@@ -16,7 +16,7 @@ const Home: React.FC = () => {
             <IonTitle size="large">Consulta CEP</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <App />
+        <ConsultaCep />
       </IonContent>
     </IonPage>
   );
