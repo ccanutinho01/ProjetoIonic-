1e4b29a957a2503620c347c9afd12eef0e796ae2
ccanutinho01 <ccanutinho01@gmail.com>
2026-07-13 16:43:04 -0300

    implementação no backend

--- backend/ServerAuth.js
+++ backend/ServerAuth.js
@@ -2,23 +2,43 @@ const express = require("express");
 
 const app = express();
 
-app.get("/usuarios", (req, res) => {
+const produtos = [
+  {
+    nome: "Notebook",
+    quantidade: 5,
+    valorUnitario: 3500,
+  },
+  {
+    nome: "Mouse",
+    quantidade: 12,
+    valorUnitario: 89.9,
+  },
+  {
+    nome: "Teclado",
+    quantidade: 8,
+    valorUnitario: 159.9,
+  },
+];
 
-    res.json([{
-        id: 1,
-        nome: "João",
-        email: "joao@email.com"
-    },
-
-    {
-        id: 2,
-        nome: "Maria",
-        email: "maria@email.com"
-    }
-]);
-
+app.get("/produtos", (req, res) => {
+  res.json(produtos);
 });
 
-app.listen(3000);
+app.get("/usuarios", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      nome: "João",
+      email: "joao@email.com",
+    },
+    {
+      id: 2,
+      nome: "Maria",
+      email: "maria@email.com",
+    },
+  ]);
+});
 
-console.log("Servidor rodando na porta 3000");
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
